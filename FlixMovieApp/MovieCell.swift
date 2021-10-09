9074e412a590746d7b041eb98351e4d54d266c9e
Luis Miguel <9luismiguel@gmail.com>
2021-10-08 23:45:21 -0700

    App completion
    The completion of my app.

---
+++ FlixMovieApp/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  FlixMovieApp
+//
+//  Created by Luis Miguel on 10/8/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
