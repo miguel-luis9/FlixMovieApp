578483f23473c556c9bf9e8d5352f032ab415cb3
Luis Miguel <9luismiguel@gmail.com>
2021-10-15 17:03:37 -0700

    added GridView screen
    Completed Unit 2: Added a new screen that shows Superhero movies as well as a MovieDetail screen when clicking on a movie

---
+++ FlixMovieApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixMovieApp
+//
+//  Created by Luis Miguel on 10/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
